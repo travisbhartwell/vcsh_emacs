726345c66a94abdd653f85778e548b1e1a93438b
Travis B. Hartwell <nafai@travishartwell.net>
2018-12-20 14:21:34 -0800

    Checkpoint Spacemacs config

---
+++ .spacemacs.d/layers/tbh-org/layers.el
@@ -0,0 +1,8 @@
+;;; layers.el --- tbh-org layers File for Spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+
+(configuration-layer/declare-layers '(
+                                      org
+                                      ))
